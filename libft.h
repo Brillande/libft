d5c88f2fc5eca486bdfaa2893ee93916309e3fb1
Brillande <jelland90@gmail.com>
2023-04-19 15:31:55 +0200

    en progreso

---
+++ libft.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+void ft_memset(void *b, int c, size_t len);
